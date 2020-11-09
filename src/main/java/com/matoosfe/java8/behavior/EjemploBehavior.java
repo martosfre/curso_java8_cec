4dee6d5fc6a4f5c32d8325fac92817b0f99ce275
martosfre <mtoscano@matoosfe.com>
2020-11-09 18:34:25 -0500

    Behavior con Lambda

--- src/main/java/com/matoosfe/java8/behavior/EjemploBehavior.java
+++ src/main/java/com/matoosfe/java8/behavior/EjemploBehavior.java
@@ -14,7 +14,6 @@ import com.matoosfe.java8.Estudiante;
  *
  */
 public class EjemploBehavior {
-	
 
 	/**
 	 * @param args
@@ -24,28 +23,31 @@ public class EjemploBehavior {
 				new Estudiante(2, "Evelyn", "Naranjo", 12, null), new Estudiante(3, "Marlon", "Carrasco", 24, null),
 				new Estudiante(4, "Oscar", "Bravo", 19, null), new Estudiante(5, "Geovanny", "Artes", 26, null),
 				new Estudiante(6, "Marlon", "Vizuete", 19, null), new Estudiante(7, "Marco", "Tipán", 11, null));
-		
+
 		System.out.println("Filtrar estudiantes mayores de edad");
 		List<Estudiante> listaEst = ControladorEstudiante.filtarPorEdad(estudiantes);
 		for (Estudiante estudiante : listaEst) {
 			System.out.println(estudiante);
 		}
-		
+
 		System.out.println("\nFiltrar estudiants mayores de edad y con nombre que comience M");
 		List<Estudiante> listaEstDos = ControladorEstudiante.filtarPorEdadNombre(estudiantes);
 		for (Estudiante estudiante : listaEstDos) {
 			System.out.println(estudiante);
 		}
-		
+
 		System.out.println("\n\nFiltrar utilizando Behavior Parametrization");
+		System.out.println("Imprimir directamente");
+		ControladorEstudiante.filtrar(estudiantes, new EdadNombreEstudiante()).forEach(System.out::println);
+		System.out.println("\nImprimir con un for each");
 		List<Estudiante> listaEstTres = ControladorEstudiante.filtrar(estudiantes, new EdadNombreEstudiante());
 		for (Estudiante estudiante : listaEstTres) {
 			System.out.println(estudiante);
 		}
-		
+
 		System.out.println("\n\nFiltrar utilizando Behavior Parametrization con Clases Anónimas");
 		List<Estudiante> listaEstCuatro = ControladorEstudiante.filtrar(estudiantes, new PredicadoEstudiante() {
-			
+
 			@Override
 			public boolean filtrar(Estudiante est) {
 				return est.getApellidoEst().startsWith("T");
@@ -54,6 +56,11 @@ public class EjemploBehavior {
 		for (Estudiante estudiante : listaEstCuatro) {
 			System.out.println(estudiante);
 		}
+		
+		System.out.println("\nFiltrar utilizando Lambda");
+		List<Estudiante> listaEstCinco = ControladorEstudiante.filtrar(estudiantes, (Estudiante est) -> est.getEdadEst() < 18);
+		listaEstCinco.forEach(System.out::println);
+		
 
 	}
 
